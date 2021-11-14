28d63ee01a3741781f5232e3cba38908f156e1e7
rupesss-09 <rupesss09@gmail.com>
2021-11-14 17:04:09 -0600

    Using Route for routing and added some pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Favorites from "./components/pages/Favorites";
+import NewMeetups from "./components/pages/NewMeetups";
+import AllMeetups from "./components/pages/AllMeetups";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className='App'>
+      <Route path='/'>
+        <AllMeetups />
+      </Route>
+      <Route path='/favorites'>
+        <Favorites />
+      </Route>
+      <Route path='/newmeetups'>
+        <NewMeetups />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/pages/AllMeetups.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllMeetups = () => {
+  return <div>AllMeetUps</div>;
+};
+
+export default AllMeetups;
