094a73258f2841acc833e5f486026946b97eb8ae
rupesss-09 <rupesss09@gmail.com>
2021-11-28 21:08:57 -0600

    Added dummyarray in Meetup.js and rendering that array object in list

--- src/components/pages/AllMeetups.js
+++ src/components/pages/AllMeetups.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React from 'react';
+
+const DUMMY_DATA = [
+    {
+        id: 'm1',
+        title: 'This is a first meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Meetupstreet 5, 12345 Meetup City',
+        description:
+            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+    },
+    {
+        id: 'm2',
+        title: 'This is a second meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Meetupstreet 5, 12345 Meetup City',
+        description:
+            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+    },
+];
 
 const AllMeetups = () => {
-  return <div className='allmeet'>AllMeetUps</div>;
+    return (
+        <div className='allmeet'>
+            <h1>All Meetup's </h1>
+            <ul>
+                {DUMMY_DATA.map((data) => {
+                    return <li key={data.id}>{data.title}</li>;
+                })}
+            </ul>
+        </div>
+    );
 };
 
 export default AllMeetups;
