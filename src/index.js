814750a180827f1c0d6848b27282aad2e81d252a
rupesss-09 <rupesss09@gmail.com>
2021-12-02 11:40:53 -0600

    Created context in store and functions handler

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import FavoriteContextProvider from 'store/favorites-context';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+    <FavoriteContextProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </FavoriteContextProvider>,
+    document.getElementById('root')
 );
---
+++ src/store/favorites-context.js
@@ -0,0 +1,41 @@
+import { useState, createContext } from 'react';
+
+const FavoriteContext = createContext({
+    favorites: [],
+    totalFavorites: 0,
+});
+
+const FavoriteContextProvider = ({ children }) => {
+    const [userFavorites, setUserFavorites] = useState([]);
+
+    const addFavoriteHandler = (favoriteMeetup) => {
+        setUserFavorites((prevUserFavorites) => {
+            return prevUserFavorites.concat(favoriteMeetup);
+        });
+    };
+
+    const removeFavoriteHandler = (meetUpId) => {
+        setUserFavorites((prevUserFavorites) => {
+            return prevUserFavorites.filter((meetup) => meetup.id === meetUpId);
+        });
+    };
+
+    const itemIsFavoriteHandler = (meetUpId) => {
+        return userFavorites.some((meetup) => meetup.id === meetUpId);
+    };
+
+    const context = {
+        favorites: userFavorites,
+        totalFavorites: userFavorites.length,
+        addFavorite: addFavoriteHandler,
+        removeFavorite: removeFavoriteHandler,
+        itemIsFavorite: itemIsFavoriteHandler,
+    };
+    return (
+        <FavoriteContext.Provider value={context}>
+            {children}
+        </FavoriteContext.Provider>
+    );
+};
+
+export default FavoriteContext;
