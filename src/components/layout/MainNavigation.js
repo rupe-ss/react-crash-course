95b42211aef69506b4e204f4ee648493e5bb1d86
rupesss-09 <rupesss09@gmail.com>
2021-12-02 12:43:20 -0600

    Rendering favorites in favoritespages and displaying number

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
+import FavoriteContext from 'store/favorites-context';
 
 const MainNavigation = () => {
+    const favoriteCtx = useContext(FavoriteContext);
     return (
         <header className={classes.header}>
             <div className={classes.logo}>React Meetups</div>
@@ -16,7 +18,12 @@ const MainNavigation = () => {
                         <Link to='/newmeetup'>Add New Meetup</Link>
                     </li>
                     <li>
-                        <Link to='/favorites'>My Favorites</Link>
+                        <Link to='/favorites'>
+                            My Favorites
+                            <span className={classes.badge}>
+                                {favoriteCtx.totalFavorites}
+                            </span>
+                        </Link>
                     </li>
                 </ul>
             </nav>
